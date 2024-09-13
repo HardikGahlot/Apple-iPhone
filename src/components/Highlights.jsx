38ea5e64900e8612812b8c5bce3e7b050dcc55d6
HardikGahlot <137612570+HardikGahlot@users.noreply.github.com>
2024-09-13 14:52:37 +0530

    Add files via upload

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react"; 
+import { gsap } from "gsap"; 
+import { rightImg, watchImg } from "../utils"; 
+
+import VideoCarousel from "../constants/VideoCarousel";
+
+const Highlights = () => {
+  useEffect(() => {
+    // GSAP animations on component mount
+    gsap.to("#title", { opacity: 1, y: 0, duration: 1 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+  }, []); 
+
+  return (
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-start justify-between">
+          <h1 id="title" className="section-heading">Get the highlights.</h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
+          </div>
+        </div>
+        <VideoCarousel />
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
